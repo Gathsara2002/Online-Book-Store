a4158ad83b10efdf320bde9700c5448b4ae6fdd4
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-31 20:15:18 +0530

    Content updated

--- client/src/view/common/CartAndFav/CartAndFav.tsx
+++ client/src/view/common/CartAndFav/CartAndFav.tsx
@@ -6,13 +6,13 @@ export function CartAndFav() {
         <section className={"flex border border-red-600 w-60 h-14 mt-[80px] absolute right-0"}>
 
             {/*Favourite*/}
-            <div className={"w-1/2 flex border border-black"}>
+            <div className={"w-1/2 flex border border-black cursor-pointer"}>
                 <img src={fav} alt="fav" className={"w-10 h-10 m"}/>
                 <h3 className={"ml-2"}>0</h3>
             </div>
 
             {/*Cart*/}
-            <div className={"w-1/2 flex border border-black"}>
+            <div className={"w-1/2 flex border border-black cursor-pointer"}>
                 <img src={cart} alt="cart" className={"w-10 h-10"}/>
                 <h3 className={"ml-2"}>0</h3>
             </div>
