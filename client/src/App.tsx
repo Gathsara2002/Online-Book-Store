933932be6aa3c60c68dc4e52af4d1a5dc28b1f28
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-02 21:24:47 +0530

    Content refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {NavBar} from "./view/common/NavBar/NavBar";
 import {CartAndFav} from "./view/common/CartAndFav/CartAndFav";
-import {Home} from "./view/common/Home/Home";
+import {Home} from "./view/pages/Home/Home";
 import {Footer} from "./view/common/Footer/Footer";
 
 function App() {
