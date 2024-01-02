190fb91ad3798503453fbe23b22614217e06f27b
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-02 21:30:46 +0530

    Content updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,16 @@ import {NavBar} from "./view/common/NavBar/NavBar";
 import {CartAndFav} from "./view/common/CartAndFav/CartAndFav";
 import {Home} from "./view/pages/Home/Home";
 import {Footer} from "./view/common/Footer/Footer";
+import {About} from "./view/pages/About/About";
 
 function App() {
     return (
         <>
             <NavBar></NavBar>
             <CartAndFav></CartAndFav>
-            <Home></Home>
-            <Footer></Footer>
+            {/*<Home></Home>*/}
+            <About></About>
+            {/*<Footer></Footer>*/}
         </>
     );
 }
