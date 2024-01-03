9004269e07505fcd2f978293780f80ae08140f75
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-03 20:47:20 +0530

    About page half done

--- client/src/view/pages/About/About.tsx
+++ client/src/view/pages/About/About.tsx
@@ -1,5 +1,10 @@
+import aboutImg from '../../../assests/images/about.jpg';
 export const About = () => {
     return (
-        <></>
+        <section className={"container mt-20"}>
+            <div className={"w-1/2 h-auto border border-black flex justify-center"}>
+                <img src={aboutImg} alt="aboutImage" className={"w-2/3 mt-20"}/>
+            </div>
+        </section>
     );
 };
