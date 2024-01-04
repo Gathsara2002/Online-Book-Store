57da1fa0152dd6494ba83961f2130eeb5fdebe9b
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-04 13:04:37 +0530

    Contact page is done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import {CartAndFav} from "./view/common/CartAndFav/CartAndFav";
 import {Home} from "./view/pages/Home/Home";
 import {Footer} from "./view/common/Footer/Footer";
 import {About} from "./view/pages/About/About";
+import {Contact} from "./view/pages/Contact/Contact";
 
 function App() {
     return (
@@ -12,7 +13,8 @@ function App() {
             <NavBar></NavBar>
             <CartAndFav></CartAndFav>
             {/*<Home></Home>*/}
-            <About></About>
+            {/*<About></About>*/}
+            <Contact></Contact>
             {/*<Footer></Footer>*/}
         </>
     );
