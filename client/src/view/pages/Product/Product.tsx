15e43cdb9c4b843ff883134f51d3b79adca33e16
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-05 15:40:52 +0530

    shop page is done

--- client/src/view/pages/Product/Product.tsx
+++ client/src/view/pages/Product/Product.tsx
@@ -1,6 +1,8 @@
+import aboutImg from '../../../assests/images/about.jpg';
+
 export const Product = () => {
     return (
-        <section className={"container mt-20"}>
+        <section className={"container mt-20 mb-20"}>
 
             {/*top menu*/}
             <div className={"flex border border-red-600 mt-28 ml-10"}>
@@ -29,6 +31,133 @@ export const Product = () => {
                 <button className={"ml-8"}>Search</button>
 
             </div>
+
+            {/*books*/}
+            <div className={"grid grid-cols-4 mt-12 border-blue-700 border mr-10 ml-10 p-2 gap-8"}>
+
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+                <div className={"border-green-400 border"}>
+                    <img src={aboutImg} alt="image" className={"w-3/4 mx-auto mt-8"}/>
+                    <div className={"border-pink-400 border mt-10 grid grid-cols-2 pl-5 pr-5"}>
+                        <p className={"font-bold"}>Book</p>
+                        <p className={"text-end text-blue-500 font-bold"}>$ 10.00</p>
+                        <p>By Author</p>
+                        <p className={"text-end"}>Comic</p>
+                    </div>
+                    <button className={"mt-2 bg-green-400 w-full"}>
+                        Add To Favourite
+                    </button>
+                    <button className={"mt-2 bg-cyan-300 w-full"}>
+                        Add To Cart
+                    </button>
+                </div>
+
+            </div>
+
         </section>
     );
 };
