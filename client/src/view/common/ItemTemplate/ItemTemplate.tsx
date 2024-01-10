747c97f24fd8015d593db9c3a5ab2233c57c35a0
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-10 16:43:08 +0530

    ItemTemplate updated

--- client/src/view/common/ItemTemplate/ItemTemplate.tsx
+++ client/src/view/common/ItemTemplate/ItemTemplate.tsx
@@ -12,6 +12,7 @@ export const ItemTemplate = () => {
             {/*description*/}
             <div className={"w-2/4 border border-red-400  mt-28 mr-auto"}>
                 <div className={"ml-24 mt-8"}>
+
                     <h1 className={"text-3xl font-medium underline uppercase"}>Book name</h1>
                     <h3 className={"mt-4 text-[18px] italic"}>In stock</h3>
                     <h3 className={"mt-5 text-3xl font-medium"}>LKR 1000</h3>
@@ -19,6 +20,18 @@ export const ItemTemplate = () => {
                     <p className={"mt-3 font-bold"}>+ Language :</p>
                     <p className={"mt-3 font-bold"}>+ Author :</p>
                     <p className={"mt-3 font-bold"}>+ Genre :</p>
+
+                    <div className={"flex mt-12"}>
+                        <div
+                            className={"flex h-12 border border-blue-700 w-60 cursor-pointer rounded text-center text-2xl"}>
+                            <div className={"bg-cyan-300 flex-1 rounded"}>-</div>
+                            <div className={"bg-white flex-1 rounded"}>1</div>
+                            <div className={"bg-cyan-300 flex-1 rounded"}>+</div>
+                        </div>
+
+                        <button className={"border border-blue-700 w-60 ml-8"}> BUY NOW</button>
+                    </div>
+
                 </div>
             </div>
 
