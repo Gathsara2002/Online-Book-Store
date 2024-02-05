2c4a28000adef813db8ee69e84619e962441a0c3
Gathsara2002 <liviru2002mmg@gmail.com>
2024-02-05 11:23:46 +0530

    Nvigation bug fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import {Signup} from "./view/pages/Signup/Signup";
 import {DefaultView} from "./view/common/DefaultView/DefaultView";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AdminDashboard} from "./view/common/AdminDashboard/AdminDashboard";
+import {Dashboard} from "./view/pages/AdminPages/Dashboard/Dashboard";
+import {Products} from "./view/pages/AdminPages/Products/Products";
 
 function App() {
     return (
@@ -14,7 +16,7 @@ function App() {
                     <Route path={"/*"} Component={DefaultView}> </Route>
                     <Route path={"/login"} Component={Login}> </Route>
                     <Route path={"/signup"} Component={Signup}> </Route>
-                    <Route path={"/admin"} Component={AdminDashboard}> </Route>
+                    <Route path={"/admin/:para"} Component={AdminDashboard}> </Route>
                 </Routes>
             </BrowserRouter>
         </>
--- client/src/view/common/AdminDashboard/AdminDashboard.tsx
+++ client/src/view/common/AdminDashboard/AdminDashboard.tsx
@@ -1,90 +1,101 @@
 import profile from '../../../assests/icons/profile.png';
 import {Dashboard} from "../../pages/AdminPages/Dashboard/Dashboard";
 import {Products} from "../../pages/AdminPages/Products/Products";
-import {Link, Route, Routes} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 
 export const AdminDashboard = () => {
-    return (
-        <div>
 
-            {/*This dashboard sidebar*/}
-            <div className="bg-gray-800 text-white h-screen  flex flex-col w-1/6">
-                <div className="p-4 mt-4">
-                    <span className="text-2xl font-bold">Chapter</span>
-                    <span className="text-2xl font-bold text-green-500">ONE</span>
-                </div>
-                <ul className="flex-1">
-                    <li className="p-4 border-b border-gray-700">
-                        <Link to={"/admin"} className="flex items-center">
-                            <i className="text-xl mr-2"></i>
-                            <span className="text-lg">Dashboard</span>
-                        </Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-700">
-                        <Link to={"/admin/customer"} className="flex items-center">
-                            <i className="text-xl mr-2"></i>
-                            <span className="text-lg">Manage Customers</span>
-                        </Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-700">
-                        <Link to={"/admin/products"} className="flex items-center">
-                            <i className="text-xl mr-2"></i>
-                            <span className="text-lg">Manage Products</span>
-                        </Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-700">
-                        <Link to={"/admin/orders"} className="flex items-center">
-                            <i className="text-xl mr-2"></i>
-                            <span className="text-lg">Manage Orders</span>
-                        </Link>
-                    </li>
-                    <li className="p-4 border-b border-gray-700">
-                        <Link to={"/admin/payment"} className="flex items-center">
-                            <i className="text-xl mr-2"></i>
-                            <span className="text-lg">Manage Payments</span>
-                        </Link>
-                    </li>
-                    <li className="p-4 mt-72">
-                        <Link to={"/"} className="flex items-center">
-                            <i className="text-xl mr-2"></i>
-                            <span className="text-lg">Log Out</span>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
+        const {para} = useParams();
+        console.log(para)
 
-            {/*nav bar*/}
-            <div className={"absolute top-0 right-0 w-5/6 z-10"}>
-                {/*nav bar*/}
-                <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
-                    <div className="w-1/3 m-auto">
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            className="px-3 py-2 w-full bg-gray-700 text-white rounded-md outline-none focus:bg-gray-600"
-                        />
-                        <i className="bx bx-search text-xl"></i>
-                    </div>
-                    <div className="profile-details flex items-center">
-                        <img
-                            src={profile}
-                            alt="Profile"
-                            className="w-8 h-8 rounded-full mr-2"
-                        />
-                        <span className="admin_name text-lg">Admin</span>
-                    </div>
-                </nav>
-            </div>
+        const setComponent = () => {
+            switch (para) {
+                case 'db':
+                    return <Dashboard></Dashboard>;
+                case 'products':
+                    return <Products></Products>;
+            }
+        }
 
-            {/*main content*/}
+
+        return (
             <div>
-                <Routes>
-                    <Route path={"/admin"} Component={Dashboard}></Route>
-                    <Route path={"/admin/products"} Component={Products}></Route>
-                </Routes>
-            </div>
+                {/*This dashboard sidebar*/}
+                <div className="bg-gray-800 text-white h-screen  flex flex-col w-1/6">
+                    <div className="p-4 mt-4">
+                        <span className="text-2xl font-bold">Chapter</span>
+                        <span className="text-2xl font-bold text-green-500">ONE</span>
+                    </div>
+                    <ul className="flex-1">
+                        <li className="p-4 border-b border-gray-700">
+                            <Link to={"/admin/db"} className="flex items-center">
+                                <i className="text-xl mr-2"></i>
+                                <span className="text-lg">Dashboard</span>
+                            </Link>
+                        </li>
+                        <li className="p-4 border-b border-gray-700">
+                            <Link to={"/admin/customer"} className="flex items-center">
+                                <i className="text-xl mr-2"></i>
+                                <span className="text-lg">Manage Customers</span>
+                            </Link>
+                        </li>
+                        <li className="p-4 border-b border-gray-700">
+                            <Link to={"/admin/products"} className="flex items-center">
+                                <i className="text-xl mr-2"></i>
+                                <span className="text-lg">Manage Products</span>
+                            </Link>
+                        </li>
+                        <li className="p-4 border-b border-gray-700">
+                            <Link to={"/admin/orders"} className="flex items-center">
+                                <i className="text-xl mr-2"></i>
+                                <span className="text-lg">Manage Orders</span>
+                            </Link>
+                        </li>
+                        <li className="p-4 border-b border-gray-700">
+                            <Link to={"/admin/payment"} className="flex items-center">
+                                <i className="text-xl mr-2"></i>
+                                <span className="text-lg">Manage Payments</span>
+                            </Link>
+                        </li>
+                        <li className="p-4 mt-72">
+                            <Link to={"/"} className="flex items-center">
+                                <i className="text-xl mr-2"></i>
+                                <span className="text-lg">Log Out</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
 
-        </div>
-    );
-};
+                {/*nav bar*/}
+                <div className={"absolute top-0 right-0 w-5/6 z-10"}>
+                    {/*nav bar*/}
+                    <nav className="flex justify-between items-center bg-gray-800 text-white p-4">
+                        <div className="w-1/3 m-auto">
+                            <input
+                                type="text"
+                                placeholder="Search..."
+                                className="px-3 py-2 w-full bg-gray-700 text-white rounded-md outline-none focus:bg-gray-600"
+                            />
+                            <i className="bx bx-search text-xl"></i>
+                        </div>
+                        <div className="profile-details flex items-center">
+                            <img
+                                src={profile}
+                                alt="Profile"
+                                className="w-8 h-8 rounded-full mr-2"
+                            />
+                            <span className="admin_name text-lg">Admin</span>
+                        </div>
+                    </nav>
+                </div>
+
+                {/*main content*/}
+                <div>
+                    {setComponent()}
+                </div>
+
+            </div>
+        );
+    }
+;
