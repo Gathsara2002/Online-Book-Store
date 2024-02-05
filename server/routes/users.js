4f720a0c46b3a0c82deb942fee2920593b41b1fa
Gathsara2002 <liviru2002mmg@gmail.com>
2024-02-05 17:14:53 +0530

    backend updated

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let user = require('../controller/UserController');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
+router.get();
+
+// save user
+router.post('/save',user.saveUser);
 
 module.exports = router;
