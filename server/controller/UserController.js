31c433d9d9c304632b619154cecd09c5a5e99854
Gathsara2002 <liviru2002mmg@gmail.com>
2024-02-13 20:53:17 +0530

    UserController updated

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -35,14 +35,68 @@ const UserController = {
 
     //delete user
     deleteUser: async function (req, res, next) {
+        try {
+
+            const id = req.params.id;
+            const user = await model.deleteOne({id: id});
+
+            if (user.deletedCount === 0) {
+                return res.status(404).json({
+                    error: "User not found !"
+                });
+            }
+
+            res.status(200).json(user);
+
+        } catch (error) {
+            res.status(500).json({
+                error: "Something went wrong ! " + error
+            });
+        }
     },
 
     //update user
     updateUser: async function (req, res, next) {
+        try {
+
+            const id = req.params.id;
+            const body = req.body;
+
+            const updatedUser = await model.findOneAndUpdate({
+                    id: id,
+                }, body,
+                {new: true}
+            );
+
+            if (!updatedUser) {
+                return res.status(404).json({
+                    error: "User not found !"
+                });
+            }
+
+            res.status(200).json(updatedUser);
+
+        } catch (error) {
+            res.status(500).json({
+                error: "Something went wrong ! " + error
+            });
+        }
+
     },
 
     //search user
     searchUser: async function (req, res, next) {
+        try {
+
+            const id = req.params.id;
+            const user = await model.find({id: id});
+            res.status(200).json(user);
+
+        } catch (error) {
+            res.status(500).json({
+                error: "Something went wrong ! " + error
+            });
+        }
     }
 
 }
